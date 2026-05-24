1d31b3906aa947269883447731acabf25c230ea5
Georg Graf (SPG) <grafg@spengergasse.at>
2026-05-24 11:12:35 +0200

    #202 first migration

---
+++ prisma/translations/migrations/20260524091203_init/migration.sql
@@ -0,0 +1,54 @@
+-- CreateTable
+CREATE TABLE "languages" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "posix_code" TEXT NOT NULL,
+    "bcp_47" TEXT NOT NULL,
+    "iso_639_3" TEXT NOT NULL,
+    "whisper_response" TEXT,
+    "directory" TEXT NOT NULL,
+    "english_name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "master_strings" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "text" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "llm_models" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "votes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "confidence" INTEGER NOT NULL,
+    "translation" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "languagesId" INTEGER NOT NULL,
+    "llm_modelsId" INTEGER NOT NULL,
+    "master_stringsId" INTEGER NOT NULL,
+    CONSTRAINT "votes_languagesId_fkey" FOREIGN KEY ("languagesId") REFERENCES "languages" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "votes_llm_modelsId_fkey" FOREIGN KEY ("llm_modelsId") REFERENCES "llm_models" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "votes_master_stringsId_fkey" FOREIGN KEY ("master_stringsId") REFERENCES "master_strings" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "languages_posix_code_key" ON "languages"("posix_code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "languages_bcp_47_key" ON "languages"("bcp_47");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "languages_directory_key" ON "languages"("directory");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "master_strings_text_key" ON "master_strings"("text");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "llm_models_name_key" ON "llm_models"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "votes_languagesId_llm_modelsId_master_stringsId_translation_key" ON "votes"("languagesId", "llm_modelsId", "master_stringsId", "translation");
